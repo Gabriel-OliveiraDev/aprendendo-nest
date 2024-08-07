27dceb03819cc8e31de4aba8e53a90d8b529857b
Gabriel Oliveira <gabrieloliveira663@icloud.com>
2024-08-07 09:50:31 -0300

    initial commit

---
+++ src/task/task.service.ts
@@ -0,0 +1,54 @@
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { FindAllParams, TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+
+  private tasks: TaskDto[] = [];
+
+  create(task: TaskDto) {
+    this.tasks.push(task)
+    console.log(this.tasks)
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter(t => t.id == id)
+    if (foundTask.length) {
+      return foundTask[0]
+    }
+    throw new HttpException(`Task with ${id} not found`, HttpStatus.BAD_REQUEST)
+  }
+
+  findAll(params: FindAllParams): TaskDto[] {
+    return this.tasks.filter(t => {
+      let match = true
+
+      if (params.title != undefined && t.title.includes(params.title)) {
+        match = false
+      }
+
+      if(params.status != undefined && t.status.includes(params.status)) {
+        match = false
+      }
+      return match
+    })
+  }
+
+  update(task: TaskDto) {
+    const index = this.tasks.findIndex(t => t.id === task.id)
+    if (index >= 0) {
+      this.tasks[index] = task
+      return
+    }
+    throw new HttpException(`Task with ${task.id} not found`, HttpStatus.BAD_REQUEST)
+  }
+
+  delete(id: string) {
+    const index = this.tasks.findIndex(t => t.id === id)
+    if (index >= 0) {
+      this.tasks.splice(index, 1)
+      return
+    }
+    throw new HttpException(`Task with ${id} not found`, HttpStatus.BAD_REQUEST)
+  }
+}
